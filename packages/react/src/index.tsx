04b7e3b750395012b52c3c366f1c5d2b4a3b5325
Kris Urbas <605420+krzysu@users.noreply.github.com>
2022-12-09 09:47:21 +0100

    try to export mocks from a separate folder

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -6,6 +6,8 @@ import {
 } from '@lens-protocol/api';
 import { ReactNode } from 'react';
 
+import { mock32BytesHexString } from '@lens-protocol/shared-kernel/tests';
+
 export function useProfilesToFollow() {
   return useProfilesToFollowQuery();
 }
@@ -20,6 +22,8 @@ export function LensClient({ children }: LensClientProps) {
   const client = createApolloClient({
     backendURL: 'https://api.lens.dev',
   });
+
+  mock32BytesHexString();
 
   return <ApolloProvider client={client}>{children}</ApolloProvider>;
 }
