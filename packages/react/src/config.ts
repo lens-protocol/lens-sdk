c507f71bc69b1d75b1473309af49777ba153d3a8
Cesare Naldi <naldi.cesare@gmail.com>
2022-12-16 15:56:02 +0100

    Reshape LensConfig

--- packages/react/src/config.ts
+++ packages/react/src/config.ts
@@ -1,14 +1,17 @@
 import { IStorageProvider } from '@lens-protocol/storage';
-import { providers } from 'ethers';
 
 import { EnvironmentConfig } from './environments';
+import { IProviderBinding } from './wallet/infrastructure/ProviderFactory';
+import { ISignerBinding } from './wallet/infrastructure/SignerFactory';
 
 export * from './environments';
 export * from './sources';
 
+export interface IBindings extends ISignerBinding, IProviderBinding {}
+
 export type LensConfig = {
   environment: EnvironmentConfig;
-  provider: providers.BaseProvider;
   storage: IStorageProvider;
   sources?: string[];
+  bindings: IBindings;
 };
