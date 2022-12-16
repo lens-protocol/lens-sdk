eb4c4f71254506569d4287a8b0c3a8250517e517
Reece Johnson <reece.robert.johnson@gmail.com>
2022-12-16 09:18:37 +0000

    feat: initial pagination implementation

---
+++ examples/web-wagmi/src/hooks/useInfiniteScroll.ts
@@ -0,0 +1,23 @@
+import { LensResponseWithPagination } from '@lens-protocol/react';
+import { useInView } from 'react-cool-inview';
+
+import { nonErrored } from '../utils/nonErrored';
+
+export function useInfiniteScroll<T>(queryResult: LensResponseWithPagination<T>) {
+  const nonErroredQueryResult = nonErrored(queryResult);
+
+  const { observe: observeRef } = useInView({
+    // Grow the root margin so the data will be loaded earlier
+
+    onEnter: async ({ unobserve, observe }) => {
+      unobserve();
+      await nonErroredQueryResult.next({
+        variables: { cursor: nonErroredQueryResult.nextCursor },
+      });
+
+      observe();
+    },
+  });
+
+  return { ...nonErroredQueryResult, observeRef };
+}
--- packages/react/src/helpers.ts
+++ packages/react/src/helpers.ts
@@ -1,5 +1,18 @@
-import { QueryResult } from '@apollo/client';
+import { ApolloError, QueryResult } from '@apollo/client';
+import { CommonPaginatedResultInfoFragment } from '@lens-protocol/api';
 import { CausedError } from '@lens-protocol/shared-kernel';
+
+function buildLensResponse<T>(
+  data: T | undefined,
+  loading: boolean,
+  error: ApolloError | undefined,
+): LensResponse<T> {
+  return {
+    data: data ?? null,
+    loading,
+    error: error ? new CausedError(error.message, { cause: error }) : null,
+  };
+}
 
 export type LensResponse<T> = {
   data: T | null;
@@ -12,9 +25,42 @@ export function useLensResponse<T, V>({
   data,
   loading,
 }: QueryResult<T, V>): LensResponse<T> {
+  return buildLensResponse<T>(data, loading, error);
+}
+
+export type PaginatedArgs<T> = T & {
+  limit?: number;
+  cursor?: string;
+};
+
+export type LensResponseWithPagination<T> = LensResponse<T> & {
+  totalCount: number;
+  previousCursor: string | null;
+  nextCursor: string | null;
+  next: (options: {
+    variables: {
+      offset?: number;
+      cursor: string | null;
+    };
+  }) => Promise<LensResponse<T>>;
+};
+
+export function useLensResponseWithPagination<
+  K,
+  T extends { result: { pageInfo: CommonPaginatedResultInfoFragment; items: K } },
+  V,
+>({ error, data, loading, fetchMore }: QueryResult<T, V>): LensResponseWithPagination<K> {
   return {
-    data: data ?? null,
-    loading,
-    error: error ? new CausedError(error.message, { cause: error }) : null,
+    ...buildLensResponse<K>(data?.result.items, loading, error),
+    totalCount: data?.result.pageInfo.totalCount ?? 0,
+    previousCursor: data?.result.pageInfo.prev ?? null,
+    nextCursor: data?.result.pageInfo.next ?? null,
+    next: async (options) => {
+      return fetchMore({
+        variables: {
+          cursor: options.variables.cursor,
+        },
+      }).then((res) => buildLensResponse<K>(res.data.result.items, res.loading, res.error));
+    },
   };
 }
