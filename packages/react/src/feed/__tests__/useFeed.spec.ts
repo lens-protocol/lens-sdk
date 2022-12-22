3b2341dc9d613ec7239d0ee1a298109b60584c4c
Reece Johnson <reece.robert.johnson@gmail.com>
2022-12-22 10:31:06 +0000

    feat: add missing tests

---
+++ packages/react/src/feed/__tests__/useFeed.spec.ts
@@ -0,0 +1,42 @@
+import {
+  createMockApolloClientWithMultipleResponses,
+  mockFeedItem,
+  mockFeedQuery,
+} from '@lens-protocol/api/mocks';
+import { waitFor } from '@testing-library/react';
+
+import { renderHookWithMocks } from '../../__helpers__/testing-library';
+import { useFeed } from '../useFeed';
+
+describe('Given the useFeed hook', () => {
+  const profileId = '0x2001';
+  const mockFeed = [mockFeedItem({}), mockFeedItem({})];
+
+  describe('when the query returns data successfully', () => {
+    it('should return the feed', async () => {
+      const { result } = renderHookWithMocks(
+        () =>
+          useFeed({
+            profileId,
+          }),
+        {
+          mocks: {
+            apolloClient: createMockApolloClientWithMultipleResponses([
+              mockFeedQuery({
+                variables: {
+                  profileId,
+                  limit: 10,
+                },
+                items: mockFeed,
+              }),
+            ]),
+          },
+        },
+      );
+
+      await waitFor(() => expect(result.current.loading).toBeFalsy());
+
+      expect(result.current.data).toEqual(mockFeed);
+    });
+  });
+});
---
+++ packages/react/src/publication/__tests__/usePublications.spec.ts
@@ -0,0 +1,42 @@
+import { PostFragment } from '@lens-protocol/api';
+import {
+  createMockApolloClientWithMultipleResponses,
+  mockPost,
+  mockPublicationsQuery,
+} from '@lens-protocol/api/mocks';
+import { waitFor } from '@testing-library/react';
+
+import { renderHookWithMocks } from '../../__helpers__/testing-library';
+import { usePublications } from '../usePublications';
+
+describe('Given the usePublications hook', () => {
+  const mockPublication: PostFragment = mockPost();
+
+  describe('when the query returns data successfully', () => {
+    it('should return publications', async () => {
+      const { result } = renderHookWithMocks(
+        () =>
+          usePublications({
+            profileId: mockPublication.profile.id,
+          }),
+        {
+          mocks: {
+            apolloClient: createMockApolloClientWithMultipleResponses([
+              mockPublicationsQuery({
+                variables: {
+                  profileId: mockPublication.profile.id,
+                  limit: 10,
+                },
+                publications: [mockPublication],
+              }),
+            ]),
+          },
+        },
+      );
+
+      await waitFor(() => expect(result.current.loading).toBeFalsy());
+
+      expect(result.current.data).toEqual([mockPublication]);
+    });
+  });
+});
