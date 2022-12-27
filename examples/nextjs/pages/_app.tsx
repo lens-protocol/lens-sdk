78c8ee3c7f1f5752ab84248726a586b6cb8e459b
Cesare Naldi <naldi.cesare@gmail.com>
2022-12-27 12:23:21 +0100

    Fixes export config and improves NextJS example

--- examples/nextjs/pages/_app.tsx
+++ examples/nextjs/pages/_app.tsx
@@ -1,6 +1,33 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@lens-protocol/example-shared';
+import { LensConfig, LensProvider, sources, staging } from '@lens-protocol/react';
+import { localStorage } from '@lens-protocol/react/web';
+import { bindings as wagmiBindings } from '@lens-protocol/wagmi';
+import type { AppProps } from 'next/app';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { optimism, polygon } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { provider, webSocketProvider } = configureChains([polygon, optimism], [publicProvider()]);
+
+const client = createClient({
+  autoConnect: true,
+  provider,
+  webSocketProvider,
+});
+
+const lensConfig: LensConfig = {
+  bindings: wagmiBindings(),
+  environment: staging,
+  sources: [sources.lenster, sources.orb, 'any-other-app-id'],
+  storage: localStorage(),
+};
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={client}>
+      <LensProvider config={lensConfig}>
+        <Component {...pageProps} />
+      </LensProvider>
+    </WagmiConfig>
+  );
 }
---
+++ examples/shared/README.md
@@ -0,0 +1,13 @@
+## `@lens-protocol/example-shared`
+
+Shared code between monorepo examples.
+
+It is not published or released anywhere directly.
+
+## Usage
+
+**Install**:
+
+```bash
+$ pnpm add -D @lens-protocol/example-shared
+```
