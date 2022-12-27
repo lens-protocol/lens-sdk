42307333ca23343f5dd642871a1c12c5f3cc1d8f
Cesare Naldi <naldi.cesare@gmail.com>
2022-12-27 13:52:44 +0100

    Removes nextjs public folder

--- examples/nextjs/pages/index.tsx
+++ examples/nextjs/pages/index.tsx
@@ -27,7 +27,10 @@ export default function Home() {
       <Head>
         <title>Lens SDK - NextJS</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌿</text></svg>"
+        />
       </Head>
       <header>
         <h1>Lens SDK</h1>
