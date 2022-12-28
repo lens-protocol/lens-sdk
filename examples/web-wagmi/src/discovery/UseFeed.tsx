add5178d68deccb1136fa7c6e8e2e8da9b7ba600
Joaquin Battilana <battilanajn@gmail.com>
2022-12-28 12:38:12 -0300

    reverted feed componment changes

--- examples/web-wagmi/src/discovery/UseFeed.tsx
+++ examples/web-wagmi/src/discovery/UseFeed.tsx
@@ -1,9 +1,28 @@
-import { isPostPublication, useFeed } from '@lens-protocol/react';
+import { FeedItemFragment, isPostPublication, useFeed } from '@lens-protocol/react';
+import { Link } from 'react-router-dom';
 
 import { Loading } from '../components/loading/Loading';
 import { useInfiniteScroll } from '../hooks/useInfiniteScroll';
 import { CreatePost } from '../publications/components/CreatePost';
 import { PublicationCard } from '../publications/components/PublicationCard';
+
+type PublicationProps = {
+  feedItem: FeedItemFragment;
+};
+
+function Publication({ feedItem: { root: publication } }: PublicationProps) {
+  return (
+    <Link
+      to={`/publication/${publication.id}`}
+      style={{
+        color: 'inherit',
+        margin: '1rem',
+      }}
+    >
+      <PublicationCard publication={publication} />
+    </Link>
+  );
+}
 
 export function Feed() {
   const infiniteScroll = useInfiniteScroll(
@@ -27,7 +46,7 @@ export function Feed() {
       {infiniteScroll.data
         .filter((i) => isPostPublication(i.root))
         .map((item, i) => (
-          <PublicationCard key={`${item.root.id}-${i}`} publication={item.root} />
+          <Publication key={`${item.root.id}-${i}`} feedItem={item} />
         ))}
 
       {infiniteScroll.hasMore && <p ref={infiniteScroll.observeRef}>Loading more...</p>}
