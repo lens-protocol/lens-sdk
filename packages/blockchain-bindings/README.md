2af390b2d9eb8663653a13a04f728e369a16b862
Reece Johnson <reece.robert.johnson@gmail.com>
2022-12-29 10:47:50 +0000

    feat: add `blockchain-bindings` package

---
+++ packages/blockchain-bindings/README.md
@@ -0,0 +1,5 @@
+# `@lens-protocol/blockchain-bindings`
+
+This package provides the tooling to interact with contracts on-chain. **It is not intended to be used directly.**
+
+**Its interface will change without notice, use it at your own risk.**
---
+++ packages/blockchain-bindings/src/types.ts
@@ -0,0 +1,16 @@
+export type ChainId = number;
+
+// https://eips.ethereum.org/EIPS/eip-3085
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export interface AddEthereumChainParameter {
+  chainId: string;
+  blockExplorerUrls?: string[];
+  chainName?: string;
+  iconUrls?: string[];
+  nativeCurrency?: {
+    name: string;
+    symbol: string;
+    decimals: number;
+  };
+  rpcUrls?: string[];
+}
