393d2fe9deaee2734001f4228929b251227d4526
pawellula <pawel@aave.com>
2023-01-06 11:46:53 +0100

    Add `transpilePackages` flag to next.js example

--- examples/nextjs/next.config.js
+++ examples/nextjs/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Until the @apollo-client fixes the ESM modules support (https://github.com/apollographql/apollo-feature-requests/issues/287)
+  // it's required to either transpile the `@lens-protocol` packages or make sure they won't get `imported` during SSR.
+  transpilePackages: ['@lens-protocol/react', '@lens-protocol/wagmi'],
 };
 
 module.exports = nextConfig;
