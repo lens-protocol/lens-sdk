b7bbcc3cbdee53fa4582940248b7e5a2596e29c6
pawellula <pawel@aave.com>
2023-01-18 10:16:44 +0100

    Fix `usePublications` pagination support

--- packages/api-bindings/src/apollo/createApolloCache.ts
+++ packages/api-bindings/src/apollo/createApolloCache.ts
@@ -16,6 +16,7 @@ import { createNotificationsFieldPolicy } from './createNotificationsFieldPolicy
 import { createProfileTypePolicy } from './createProfileTypePolicy';
 import { createProfilesFieldPolicy } from './createProfilesFieldPolicy';
 import { createPublicationTypePolicy } from './createPublicationTypePolicy';
+import { createPublicationsFieldPolicy } from './createPublicationsFieldPolicy';
 import { createSearchFieldPolicy } from './createSearchFieldPolicy';
 import { createWhoReactedPublicationFieldPolicy } from './createWhoReactedPublicationFieldPolicy';
 
@@ -65,7 +66,7 @@ function createTypePolicies(): TypePolicies {
         explorePublications: createExplorePublicationsFieldPolicy(),
         notifications: createNotificationsFieldPolicy(),
         profiles: createProfilesFieldPolicy(),
-        publications: createPublicationTypePolicy(),
+        publications: createPublicationsFieldPolicy(),
         search: createSearchFieldPolicy(),
         whoReactedPublication: createWhoReactedPublicationFieldPolicy(),
       },
