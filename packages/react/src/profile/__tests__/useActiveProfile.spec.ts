60dd42cf45a6f1325e8f7c57f4c8254659628629
Reece Johnson <34844044+reecejohnson@users.noreply.github.com>
2023-01-30 11:53:00 +0000

    fix: logout error between wagmi and lens-sdk (#122)
    * fix: logout error between wagmi and lens-sdk
    
    * feat: rework method to retreive active profile
    
    * feat: rework method to retreive active profile
    
    * fix: logout error between wagmi and lens-sdk

---
+++ packages/react/src/profile/__tests__/useActiveProfile.spec.ts
@@ -0,0 +1,82 @@
+import {
+  createMockApolloClientWithMultipleResponses,
+  mockGetProfileQueryMockedResponse,
+  mockProfileFragment,
+} from '@lens-protocol/api-bindings/mocks';
+import { ProfileIdentifier } from '@lens-protocol/domain/dist/use-cases/profile';
+import { waitFor } from '@testing-library/react';
+
+import { renderHookWithMocks } from '../../__helpers__/testing-library';
+import { activeProfileVar } from '../adapters/ActiveProfilePresenter';
+import { useActiveProfile } from '../useActiveProfile';
+import { ProfileFragment } from '../useProfilesToFollow';
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+jest.mock('../../lifecycle/adapters/ApplicationPresenter', () => ({
+  ...jest.requireActual('../../lifecycle/adapters/ApplicationPresenter'),
+  useAppState: jest.fn().mockReturnValue('READY'),
+}));
+
+function setupUseActiveProfile(args: {
+  profile: ProfileFragment;
+  activeProfile: ProfileIdentifier | null;
+}) {
+  activeProfileVar(args.activeProfile);
+
+  return renderHookWithMocks(() => useActiveProfile(), {
+    mocks: {
+      apolloClient: createMockApolloClientWithMultipleResponses([
+        mockGetProfileQueryMockedResponse({
+          profile: args.profile,
+          request: {
+            profileId: args.profile.id,
+          },
+        }),
+      ]),
+    },
+  });
+}
+
+describe(`Given the ${useActiveProfile.name} hook`, () => {
+  const profileId = '0x2000';
+  const handle = 'aave.lens';
+  const mockProfile: ProfileFragment = mockProfileFragment({ id: profileId, handle });
+
+  describe('when the fetch active profile query is loading', () => {
+    it('should return loading', async () => {
+      const { result } = setupUseActiveProfile({
+        profile: mockProfile,
+        activeProfile: { handle, id: profileId },
+      });
+
+      expect(result.current.loading).toBeTruthy();
+      expect(result.current.data).toBeUndefined();
+    });
+  });
+
+  describe('when the active profile is defined', () => {
+    it('should return a profile', async () => {
+      const { result } = setupUseActiveProfile({
+        profile: mockProfile,
+        activeProfile: { handle, id: profileId },
+      });
+
+      await waitFor(() => expect(result.current.loading).toBeFalsy());
+
+      expect(result.current.data).toEqual(mockProfile);
+    });
+  });
+
+  describe('when the active profile is not defined', () => {
+    it('should return null', async () => {
+      const { result } = setupUseActiveProfile({
+        profile: mockProfile,
+        activeProfile: null,
+      });
+
+      await waitFor(() => expect(result.current.loading).toBeFalsy());
+
+      expect(result.current.data).toBeNull();
+    });
+  });
+});
--- packages/react/src/profile/useActiveProfile.ts
+++ packages/react/src/profile/useActiveProfile.ts
@@ -1,15 +1,27 @@
-import { ProfileFragment } from '@lens-protocol/api-bindings';
+import { ProfileFragment, useGetProfileQuery } from '@lens-protocol/api-bindings';
 
 import { ReadResult } from '../helpers';
 import { ApplicationsState, useAppState } from '../lifecycle/adapters/ApplicationPresenter';
+import { useSharedDependencies } from '../shared';
 import { useActiveProfileVar } from './adapters/ActiveProfilePresenter';
 
 export function useActiveProfile(): ReadResult<ProfileFragment | null, void> {
   const state = useAppState();
-
+  const { apolloClient } = useSharedDependencies();
   const profile = useActiveProfileVar();
 
-  if (state === ApplicationsState.LOADING) {
+  const { data, loading } = useGetProfileQuery({
+    variables: {
+      request: {
+        profileId: profile?.id,
+      },
+    },
+    fetchPolicy: 'cache-first',
+    skip: state !== ApplicationsState.READY || !profile,
+    client: apolloClient,
+  });
+
+  if (loading) {
     return {
       data: undefined,
       loading: true,
@@ -17,7 +29,7 @@ export function useActiveProfile(): ReadResult<ProfileFragment | null, void> {
   }
 
   return {
-    data: profile ?? null,
+    data: data?.result ?? null,
     loading: false,
   };
 }
