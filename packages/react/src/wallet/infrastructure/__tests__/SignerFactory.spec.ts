c39368fcc54e7ac5b597a33d8ecfdb9bd8a017da
Paul Xu <paulxuca@gmail.com>
2023-02-06 23:49:37 -0500

    Add test

--- packages/react/src/wallet/infrastructure/__tests__/SignerFactory.spec.ts
+++ packages/react/src/wallet/infrastructure/__tests__/SignerFactory.spec.ts
@@ -1,7 +1,7 @@
 import { WalletConnectionError, WalletConnectionErrorReason } from '@lens-protocol/domain/entities';
 import { ChainType } from '@lens-protocol/shared-kernel';
 import { mockEthereumAddress } from '@lens-protocol/shared-kernel/mocks';
-import { errors, logger, providers, utils, VoidSigner } from 'ethers';
+import { errors, Wallet, logger, providers, utils, VoidSigner } from 'ethers';
 import { mock } from 'jest-mock-extended';
 import { when } from 'jest-when';
 
@@ -124,6 +124,24 @@ describe(`Given an instance of the ${SignerFactory.name}`, () => {
 
         const error = new WalletConnectionError(WalletConnectionErrorReason.INCORRECT_CHAIN);
         expect(() => result.unwrap()).toThrowError(error);
+      });
+    });
+
+    describe('with a wallet', () => {
+      it('should retrieve the signer', async () => {
+        // This is the private key of the `@jsisthebest.test` profile
+        // It's a public private key so anyone can modify the profile
+        // For your own convenience change to the private key of a new wallet
+        const testWalletPrivateKey =
+          '6c434da5e5c0e3a8e0db5cf835d23e04c7592037854f0700c26836be7581c68c';
+
+        const wallet = new Wallet(testWalletPrivateKey);
+
+        const { signerFactory } = setupTestScenario({ signer: wallet });
+
+        const result = await signerFactory.createSigner({ address: wallet.address });
+
+        expect(result.unwrap()).toBe(wallet);
       });
     });
   });
