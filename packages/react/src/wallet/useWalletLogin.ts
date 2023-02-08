fdd5c8a8fb4b5a957b9f3f45f1b4e7824e10cfcd
Cesare Naldi <naldi.cesare@gmail.com>
2023-02-08 23:12:18 +0100

    Amends useWalletLogin and useWalletLogout to leverage useOperation

--- packages/react/src/wallet/useWalletLogin.ts
+++ packages/react/src/wallet/useWalletLogin.ts
@@ -1,40 +1,16 @@
-import {
-  PendingSigningRequestError,
-  UserRejectedError,
-  WalletConnectionError,
-} from '@lens-protocol/domain/entities';
 import { Signer } from 'ethers';
-import { useState } from 'react';
 
+import { useOperation } from '../helpers';
 import { useWalletLoginController } from './adapters/useWalletLoginController';
 
 export function useWalletLogin() {
-  const [error, setError] = useState<
-    PendingSigningRequestError | UserRejectedError | WalletConnectionError | null
-  >(null);
-  const [isPending, setIsPending] = useState(false);
   const loginWallet = useWalletLoginController();
 
-  return {
-    login: async (signer: Signer) => {
-      setError(null);
-      setIsPending(true);
+  return useOperation(async (signer: Signer) => {
+    const address = await signer.getAddress();
 
-      try {
-        const address = await signer.getAddress();
-
-        const result = await loginWallet({
-          address,
-        });
-
-        if (result.isFailure()) {
-          setError(result.error);
-        }
-      } finally {
-        setIsPending(false);
-      }
-    },
-    error,
-    isPending,
-  };
+    return loginWallet({
+      address,
+    });
+  });
 }
