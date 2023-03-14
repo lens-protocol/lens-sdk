2d4a7160b09260b59cb5cdbcad1bf9be5e462242
Cesare Naldi <naldi.cesare@gmail.com>
2023-03-14 18:36:01 +0100

    Refactors and amends useWhoReacted hook

--- packages/react/src/publication/useWhoReacted.ts
+++ packages/react/src/publication/useWhoReacted.ts
@@ -2,29 +2,36 @@ import {
   WhoReactedResultFragment,
   useWhoReactedPublicationQuery,
 } from '@lens-protocol/api-bindings';
+import { PublicationId } from '@lens-protocol/domain/entities';
 
+import {
+  SubjectiveArgs,
+  useActiveProfileAsDefaultObserver,
+  useConfigSourcesVariable,
+  useLensApolloClient,
+} from '../helpers/arguments';
 import { PaginatedReadResult, PaginatedArgs, usePaginatedReadResult } from '../helpers/reads';
-import { useSharedDependencies } from '../shared';
 
-type UseWhoReactedArgs = PaginatedArgs<{
-  publicationId: string;
-  observerId?: string;
-}>;
+export type UseWhoReactedArgs = PaginatedArgs<
+  SubjectiveArgs<{
+    publicationId: PublicationId;
+  }>
+>;
 
 export function useWhoReacted(
   args: UseWhoReactedArgs,
 ): PaginatedReadResult<WhoReactedResultFragment[]> {
-  const { apolloClient, sources } = useSharedDependencies();
-
   return usePaginatedReadResult(
-    useWhoReactedPublicationQuery({
-      variables: {
-        publicationId: args.publicationId,
-        observerId: args?.observerId,
-        limit: args?.limit ?? 10,
-        sources,
-      },
-      client: apolloClient,
-    }),
+    useWhoReactedPublicationQuery(
+      useLensApolloClient(
+        useActiveProfileAsDefaultObserver({
+          variables: useConfigSourcesVariable({
+            publicationId: args.publicationId,
+            observerId: args?.observerId,
+            limit: args?.limit ?? 10,
+          }),
+        }),
+      ),
+    ),
   );
 }
