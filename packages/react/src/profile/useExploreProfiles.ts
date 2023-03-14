be43a7b63ebb039aa3f504afa805e3009f5e77cc
Cesare Naldi <naldi.cesare@gmail.com>
2023-03-14 12:42:03 +0100

    Refactors useExploreProfiles hook

--- packages/react/src/profile/useExploreProfiles.ts
+++ packages/react/src/profile/useExploreProfiles.ts
@@ -1,23 +1,21 @@
 import { ProfileFragment, useExploreProfilesQuery } from '@lens-protocol/api-bindings';
 
+import { useConfigSourcesVariable, useLensApolloClient } from '../helpers/arguments';
 import { PaginatedReadResult, PaginatedArgs, usePaginatedReadResult } from '../helpers/reads';
-import { useSharedDependencies } from '../shared';
 
 type UseFeedArgs = PaginatedArgs<{
   observerId?: string;
 }>;
 
 export function useExploreProfiles(args?: UseFeedArgs): PaginatedReadResult<ProfileFragment[]> {
-  const { apolloClient, sources } = useSharedDependencies();
-
   return usePaginatedReadResult(
-    useExploreProfilesQuery({
-      variables: {
-        observerId: args?.observerId,
-        limit: args?.limit ?? 10,
-        sources,
-      },
-      client: apolloClient,
-    }),
+    useExploreProfilesQuery(
+      useLensApolloClient({
+        variables: useConfigSourcesVariable({
+          observerId: args?.observerId,
+          limit: args?.limit ?? 10,
+        }),
+      }),
+    ),
   );
 }
