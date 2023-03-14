af1f5961d85f8c6dbdfc1610eb804edbbed21b93
Cesare Naldi <naldi.cesare@gmail.com>
2023-03-14 12:32:05 +0100

    Amends useCollectedPublications hook

--- packages/react/src/profile/useCollectedPublications.ts
+++ packages/react/src/profile/useCollectedPublications.ts
@@ -2,29 +2,41 @@ import {
   AnyPublicationFragment,
   useWalletCollectedPublicationsQuery,
 } from '@lens-protocol/api-bindings';
+import { Prettify } from '@lens-protocol/shared-kernel';
 
+import {
+  SubjectiveArgs,
+  useActiveProfileAsDefaultObserver,
+  useConfigSourcesVariable,
+  useLensApolloClient,
+} from '../helpers/arguments';
 import { PaginatedArgs, PaginatedReadResult, usePaginatedReadResult } from '../helpers/reads';
-import { useSharedDependencies } from '../shared';
 import { DEFAULT_PAGINATED_QUERY_LIMIT } from '../utils';
 
-type UseCollectablesArgs = PaginatedArgs<{
-  walletAddress: string;
-}>;
+type UseCollectablesArgs = Prettify<
+  PaginatedArgs<
+    SubjectiveArgs<{
+      walletAddress: string;
+    }>
+  >
+>;
 
 export function useCollectedPublications({
   walletAddress,
+  observerId,
   limit = DEFAULT_PAGINATED_QUERY_LIMIT,
 }: UseCollectablesArgs): PaginatedReadResult<AnyPublicationFragment[]> {
-  const { apolloClient, sources } = useSharedDependencies();
-
   return usePaginatedReadResult(
-    useWalletCollectedPublicationsQuery({
-      variables: {
-        walletAddress,
-        limit: limit,
-        sources,
-      },
-      client: apolloClient,
-    }),
+    useWalletCollectedPublicationsQuery(
+      useLensApolloClient(
+        useActiveProfileAsDefaultObserver({
+          variables: useConfigSourcesVariable({
+            walletAddress,
+            limit: limit,
+            observerId,
+          }),
+        }),
+      ),
+    ),
   );
 }
