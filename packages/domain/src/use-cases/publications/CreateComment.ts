483c205e42484bf65465d1d0c16f9190c104ed6a
Cesare Naldi <naldi.cesare@gmail.com>
2023-05-08 12:09:54 +0200

    Proposal

--- packages/domain/src/use-cases/publications/CreateComment.ts
+++ packages/domain/src/use-cases/publications/CreateComment.ts
@@ -9,70 +9,123 @@ import {
 } from '../../entities';
 import { DelegableSigning } from '../transactions/DelegableSigning';
 import { ReferencePolicyConfig } from './ReferencePolicyConfig';
-import { ImageType } from './config';
 import { CollectPolicyConfig, ContentFocus, ContentWarning, Locale, MediaObject } from './types';
 
-export type CreateCommentRequest = {
-  collect: CollectPolicyConfig;
-  decryptionCriteria?: DecryptionCriteria;
-  offChain: boolean;
-  delegate: boolean;
+export type BaseCommentRequest = {
+  /**
+   * The discriminator for the transaction kind.
+   */
   kind: TransactionKind.CREATE_COMMENT;
-  profileId: ProfileId;
-  publicationId: PublicationId;
-  reference: ReferencePolicyConfig;
-
   /**
-   * The content of a publication. If this is blank then `media` must be defined
+   * This is the appId the post belongs to.
    */
-  content?: string;
+  appId?: AppId;
   /**
-   * Main content focus for this publication
+   * The publication collect policy. Determines the criteria that must be met for a user to be able to collect the publication.
    */
-  contentFocus: ContentFocus;
+  collect: CollectPolicyConfig;
   /**
-   * IOS 639-1 language code aka en or it and ISO 3166-1 alpha-2 region code
-   * aka US or IT aka en-US or it-IT
+   * Specifies a content warning for the publication.
+   */
+  contentWarning?: ContentWarning;
+  /**
+   * The criteria that must be met for a user to be able to decrypt the publication.
+   */
+  decryptionCriteria?: DecryptionCriteria;
+  /**
+   * Whether is possible to delegate the publication signing to the profile's dispatcher.
+   */
+  delegate: boolean;
+  /**
+   * The language of the publication.
+   *
+   * It's a locale string in the format of `<language-tag>-<region-tag>` or just `<language-tag>`, where:
+   * - `language-tag` is a two-letter ISO 639-1 language code, e.g. `en` or `it`
+   * - `region-tag` is a two-letter ISO 3166-1 alpha-2 region code, e.g. `US` or `IT`
+   *
+   * You can just pass in the language tag if you do not know the region or don't need to be specific.
    */
   locale: Locale;
   /**
-   * Lens supported attached media items to the publication
+   * Whether the publication should be published off-chain (i.e. Momoka DA layer) or not.
+   */
+  offChain: boolean;
+  /**
+   * The publication author profile id.
+   */
+  profileId: ProfileId;
+  /**
+   * The publication ID to which the publication is being posted.
+   */
+  publicationId: PublicationId;
+  /**
+   * The publication reference policy. Determines the criteria that must be met for a user to be able to publication or mirror the publication.
+   */
+  reference: ReferencePolicyConfig;
+  /**
+   * A list of tags for the publication. This can be used to categorize the publication.
+   *
+   * These are not the same as #hashtag in the publication content. Use these if you don't want to clutter the publication content with tags.
+   */
+  tags?: string[];
+};
+
+export type CreateTextualCommentRequest = BaseCommentRequest & {
+  /**
+   * The publication content as Markdown string.
+   */
+  content: string;
+  /**
+   * The publication focus.
+   */
+  contentFocus: ContentFocus.ARTICLE | ContentFocus.LINK | ContentFocus.TEXT_ONLY;
+  /**
+   * The publication media. An array of media objects.
    */
   media?: MediaObject[];
+};
+
+export type CreateMediaCommentRequest = BaseCommentRequest & {
+  /**
+   * The publication content as Markdown string.
+   */
+  content?: string;
+  /**
+   * The publication focus.
+   */
+  contentFocus: ContentFocus.AUDIO | ContentFocus.IMAGE | ContentFocus.VIDEO;
+  /**
+   * The publication media. An array of media objects.
+   */
+  media: MediaObject[];
+};
+
+export type CreateEmbedCommentRequest = BaseCommentRequest & {
   /**
    * A URL to a multi-media attachment for the item. The file extensions GLTF, GLB, WEBM, MP4, M4V, OGV,
    * and OGG are supported, along with the audio-only extensions MP3, WAV, and OGA.
    * Animation_url also supports HTML pages, allowing you to build rich experiences and interactive NFTs using JavaScript canvas,
    * WebGL, and more. Scripts and relative paths within the HTML page are now supported. However, access to browser extensions is not supported.
    */
-  animationUrl?: Url;
+  animationUrl: Url;
   /**
-   * This is the appId the content belongs to
+   * The publication content as Markdown string.
    */
-  appId?: AppId;
+  content?: string;
   /**
-   * Ability to add a content warning
+   * The publication focus.
    */
-  contentWarning?: ContentWarning;
+  contentFocus: ContentFocus.EMBED;
   /**
-   * This is the URL that will appear below the asset's image on OpenSea and others
-   * and will allow users to leave OpenSea and view the item on the site.
+   * The publication media. An array of media objects.
    */
-  externalUrl?: Url;
-  /**
-   * Legacy to support OpenSea schema, store any NFT image here.
-   */
-  image?: Url;
-  /**
-   * This is the mime type of the image. This is used if you are uploading more advanced
-   * cover images as sometimes ipfs does not emit the content header so this solves that
-   */
-  imageMimeType?: ImageType;
-  /**
-   * Tag the publication to be able to search for it later
-   */
-  tags?: string[];
+  media?: MediaObject[];
 };
+
+export type CreateCommentRequest =
+  | CreateTextualCommentRequest
+  | CreateMediaCommentRequest
+  | CreateEmbedCommentRequest;
 
 export class CreateComment {
   constructor(
--- packages/domain/src/use-cases/publications/types.ts
+++ packages/domain/src/use-cases/publications/types.ts
@@ -6,29 +6,45 @@ export type Locale = string;
 
 export enum ContentFocus {
   /**
-   * The primary objective of the publication is to provide a textual content.
-   */
-  TEXT = 'TextOnly',
-  /**
-   * The primary objective of the publication is to provide an image or a gallery of images.
-   * The publication `content` field be used to add extra context to the image(s).
-   */
-  IMAGE = 'Image',
-  /**
-   * The primary objective of the publication is to provide a video or a collection of videos.
-   * The publication `content` field be used to add extra context to the video(s).
-   */
-  VIDEO = 'Video',
-  /**
-   * The primary objective of the publication is to provide an audio or a playlist of audio files.
-   * The publication `content` field be used to add extra context to the audio(s).
-   */
-  AUDIO = 'Audio',
-  /**
    * The primary objective of the publication is to provide a document-like textual content.
    * It can be accompanied by images, videos, or audio files.
    */
   ARTICLE = 'Article',
+  /**
+   * The primary objective of the publication is to provide an audio or a playlist of audio files.
+   * The publication `content` field can be used to add extra context to the audio(s).
+   */
+  AUDIO = 'Audio',
+  /**
+   * The primary objective of the publication is to provide an embedded, possibly animated or interactive content.
+   * The publication `content` and `media` fields can be used to add extra context to the embed.
+   */
+  EMBED = 'Embed',
+  /**
+   * The primary objective of the publication is to provide an image or a gallery of images.
+   * The publication `content` field can be used to add extra context to the image(s).
+   */
+  IMAGE = 'Image',
+  /**
+   * The primary objective of the publication is to provide a link to an external resource.
+   * The link URL must be provided in the `content` field.
+   */
+  LINK = 'Link',
+  /**
+   * @deprecated use {@link ContentFocus.TEXT_ONLY} instead.
+   */
+  TEXT = 'TextOnly',
+  /**
+   * The primary objective of the publication is to provide short textual content.
+   *
+   * If you want to provide a longer text, use {@link ContentFocus.ARTICLE} instead.
+   */
+  TEXT_ONLY = 'TextOnly',
+  /**
+   * The primary objective of the publication is to provide a video or a collection of videos.
+   * The publication `content` field can be used to add extra context to the video(s).
+   */
+  VIDEO = 'Video',
 }
 
 export enum ContentWarning {
@@ -61,9 +77,27 @@ export type NftAttribute =
     };
 
 export type NftMetadata = {
+  /**
+   * The name of the NFT.
+   */
   name: string;
+  /**
+   * The description of the NFT.
+   */
   description?: string;
+  /**
+   * A list of attributes for the NFT.
+   */
   attributes: NftAttribute[];
+  /**
+   * Legacy to support OpenSea schema, store any NFT image here.
+   */
+  image?: Url;
+  /**
+   * This is the mime type of the image. This is used if you are uploading more advanced
+   * cover images as sometimes IPFS does not emit the content header so this solves that.
+   */
+  imageMimeType?: ImageType;
 };
 
 export type RecipientWithSplit = {
