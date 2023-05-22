5ee680421d6cee101f29f153a89f11fe13e604a3
Kris Urbas <605420+krzysu@users.noreply.github.com>
2023-05-22 10:58:12 +0200

    feat: add verify and getAccessToken methods

--- packages/client/src/authentication/Authentication.ts
+++ packages/client/src/authentication/Authentication.ts
@@ -1,4 +1,10 @@
-import { EthereumAddress, failure, PromiseResult, success } from '@lens-protocol/shared-kernel';
+import {
+  EthereumAddress,
+  failure,
+  invariant,
+  PromiseResult,
+  success,
+} from '@lens-protocol/shared-kernel';
 import { InMemoryStorageProvider } from '@lens-protocol/storage';
 
 import { LensConfig } from '../consts/config';
@@ -34,6 +40,21 @@ export interface IAuthentication {
    * @returns Whether the user is authenticated
    */
   isAuthenticated(): Promise<boolean>;
+
+  /**
+   * Verify that the access token is signed by the server and the user.
+   *
+   * @param accessToken - The access token to verify
+   * @returns Whether the access token is valid
+   */
+  verify(accessToken: string): Promise<boolean>;
+
+  /**
+   * Get the access token. If it expired, try to refresh it.
+   *
+   * @returns The access token
+   */
+  getAccessToken(): PromiseResult<string, CredentialsExpiredError | NotAuthenticatedError>;
 }
 
 /**
@@ -79,6 +100,33 @@ export class Authentication implements IAuthentication {
 
     // credentials expired
     return false;
+  }
+
+  async verify(accessToken: string): Promise<boolean> {
+    return this.api.verify(accessToken);
+  }
+
+  async getAccessToken(): PromiseResult<string, CredentialsExpiredError | NotAuthenticatedError> {
+    const credentials = await this.storage.get();
+
+    if (!credentials) {
+      return failure(new NotAuthenticatedError());
+    }
+
+    if (!credentials.shouldRefresh() && credentials.accessToken) {
+      return success(credentials.accessToken);
+    }
+
+    if (credentials.canRefresh()) {
+      const newCredentials = await this.api.refresh(credentials.refreshToken);
+      await this.storage.set(newCredentials);
+
+      invariant(newCredentials.accessToken, 'Access token should be defined');
+      return success(newCredentials.accessToken);
+    }
+
+    // otherwise
+    return failure(new CredentialsExpiredError());
   }
 
   async getRequestHeader(): PromiseResult<
