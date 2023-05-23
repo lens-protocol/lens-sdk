ad8ffe19999c33d5444e55fa6e81ec018728c82f
Reece Johnson <reece.robert.johnson@gmail.com>
2023-05-23 10:58:51 +0100

    Revert "feat: initial pass"
    This reverts commit cbacb6768b3715904be21e4be07b166570b92d22.

--- packages/domain/src/use-cases/transactions/SignlessSubsidizeOnChain.ts
+++ packages/domain/src/use-cases/transactions/SignlessSubsidizeOnChain.ts
@@ -1,18 +1,15 @@
-import { PromiseResult, success } from '@lens-protocol/shared-kernel';
+import { success } from '@lens-protocol/shared-kernel';
 
 import {
-  AnyTransactionRequestModel,
   ProtocolTransactionRequestModel,
   ProxyTransaction,
+  AnyTransactionRequestModel,
 } from '../../entities';
 import { IGenericResultPresenter } from './IGenericResultPresenter';
 import { TransactionQueue } from './TransactionQueue';
-import { ProxyActionUsageLimitExceededError } from './ProxyActionUsageLimitExceededError';
 
 export interface ISignlessSubsidizedCallRelayer<T extends ProtocolTransactionRequestModel> {
-  createProxyTransaction(
-    request: T,
-  ): PromiseResult<ProxyTransaction<T>, ProxyActionUsageLimitExceededError>;
+  createProxyTransaction(request: T): Promise<ProxyTransaction<T>>;
 }
 
 export type ISignlessSubsidizeOnChainPresenter = IGenericResultPresenter<void, Error>;
@@ -27,12 +24,8 @@ export class SignlessSubsidizeOnChain<T extends ProtocolTransactionRequestModel>
   async execute(request: T) {
     const transaction = await this.relayer.createProxyTransaction(request);
 
-    if (transaction.isSuccess()) {
-      await this.transactionQueue.push(transaction.value);
+    await this.transactionQueue.push(transaction);
 
-      this.presenter.present(success());
-    } else {
-      // handle error
-    }
+    this.presenter.present(success());
   }
 }
