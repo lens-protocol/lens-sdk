bc4732d0f38740d8b837684e199df4f49060f4f5
Cesare Naldi <naldi.cesare@gmail.com>
2023-06-02 15:39:34 +0200

    Exposes asset factories

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -118,7 +118,7 @@ export { UnspecifiedError } from '@lens-protocol/api-bindings';
 /**
  * Helpers
  */
-export { Amount, WellKnownSymbols } from '@lens-protocol/shared-kernel';
+export { Amount, WellKnownSymbols, ether, matic, erc20, usd } from '@lens-protocol/shared-kernel';
 export * from './sources';
 export * from './utils';
 export { isValidHandle } from '@lens-protocol/api-bindings';
