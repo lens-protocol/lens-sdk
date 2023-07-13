ba00094745b9a2fb6832dc4cd3897dabe131171f
Paweł Lula <pavloblack@hotmail.com>
2023-07-13 10:11:32 +0200

    Fix `web-wagmi` build issue

--- packages/react/src/lifecycle/useCurrentSession.ts
+++ packages/react/src/lifecycle/useCurrentSession.ts
@@ -74,11 +74,7 @@ export function useCurrentSession(): ReadResult<
 > {
   const session = useSessionVar();
 
-  console.log('session', session);
-
   const prevSessionValue = usePrevious(session);
-
-  console.log('prevSessionValue', prevSessionValue);
 
   const trigger = session?.type === SessionType.WithProfile;
 
@@ -93,8 +89,6 @@ export function useCurrentSession(): ReadResult<
       skip: !trigger,
     }),
   );
-
-  console.log(data);
 
   if (!session) {
     return {
