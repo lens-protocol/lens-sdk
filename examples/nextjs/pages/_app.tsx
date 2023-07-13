442b1ef7d7a3c861958b811f19f7dfca8a58fbff
Paweł Lula <pavloblack@hotmail.com>
2023-07-13 16:35:18 +0700

    Fix `web-wagmi` build (#416)

--- examples/nextjs/pages/_app.tsx
+++ examples/nextjs/pages/_app.tsx
@@ -1,35 +1,20 @@
 import 'example-shared';
 import { LensConfig, LensProvider, sources, development, appId } from '@lens-protocol/react-web';
-import { bindings as wagmiBindings } from '@lens-protocol/wagmi';
 import type { AppProps } from 'next/app';
 import React from 'react';
-import { configureChains, createClient, WagmiConfig } from 'wagmi';
-import { polygonMumbai, polygon } from 'wagmi/chains';
-import { publicProvider } from 'wagmi/providers/public';
 
-const { provider, webSocketProvider } = configureChains(
-  [polygon, polygonMumbai],
-  [publicProvider()],
-);
-
-const client = createClient({
-  autoConnect: true,
-  provider,
-  webSocketProvider,
-});
+import { bindings } from '../domain/wallet';
 
 const lensConfig: LensConfig = {
-  bindings: wagmiBindings(),
+  bindings: bindings(),
   environment: development,
   sources: [sources.lenster, sources.orb, appId('any-other-app-id')],
 };
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <WagmiConfig client={client}>
-      <LensProvider config={lensConfig}>
-        <Component {...pageProps} />
-      </LensProvider>
-    </WagmiConfig>
+    <LensProvider config={lensConfig}>
+      <Component {...pageProps} />
+    </LensProvider>
   );
 }
