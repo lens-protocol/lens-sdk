fda8eac4dcef378b7316a1670af899937977a293
Cesare Naldi <naldi.cesare@gmail.com>
2023-07-18 11:14:27 +0200

    Fixes linting issues

--- packages/api-bindings/src/lens/__helpers__/fragments.ts
+++ packages/api-bindings/src/lens/__helpers__/fragments.ts
@@ -164,6 +164,8 @@ export function mockProfileFragment(overrides?: Partial<Profile>): Profile {
     __attributes: [],
     attributes: {} as ProfileAttributes,
 
+    invitedBy: null,
+
     ...overrides,
     __typename: 'Profile',
   };
