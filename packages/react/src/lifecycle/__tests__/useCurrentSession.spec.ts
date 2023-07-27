228b873de692049431320a5c1aa5d32d86fe8602
Kris Urbas <605420+krzysu@users.noreply.github.com>
2023-07-27 10:49:26 +0200

    improve test

--- packages/react/src/lifecycle/__tests__/useCurrentSession.spec.ts
+++ packages/react/src/lifecycle/__tests__/useCurrentSession.spec.ts
@@ -23,19 +23,21 @@ import {
 } from '../../mediaTransforms';
 import { useCurrentSession } from '../useCurrentSession';
 
-function setupUseCurrentSession({ profile = mockProfileFragment() }: { profile?: Profile } = {}) {
+function setupUseCurrentSession({ profiles }: { profiles: Profile[] }) {
   const sources = mockSources();
 
-  const apolloClient = mockLensApolloClient([
-    mockGetProfileResponse({
-      variables: {
-        request: { profileId: profile.id },
-        sources,
-        ...mediaTransformConfigToQueryVariables(defaultMediaTransformsConfig),
-      },
-      profile,
-    }),
-  ]);
+  const apolloClient = mockLensApolloClient(
+    profiles.map((profile) =>
+      mockGetProfileResponse({
+        variables: {
+          request: { profileId: profile.id },
+          sources,
+          ...mediaTransformConfigToQueryVariables(defaultMediaTransformsConfig),
+        },
+        profile,
+      }),
+    ),
+  );
 
   return renderHookWithMocks(() => useCurrentSession(), {
     mocks: { sources, mediaTransforms: defaultMediaTransformsConfig, apolloClient },
@@ -53,7 +55,7 @@ describe(`Given the ${useCurrentSession.name} hook`, () => {
     });
 
     it('should return the expected loading state', async () => {
-      const { result } = setupUseCurrentSession();
+      const { result } = setupUseCurrentSession({ profiles: [] });
 
       expect(result.current.loading).toBeTruthy();
       expect(result.current.data).toBeUndefined();
@@ -92,7 +94,7 @@ describe(`Given the ${useCurrentSession.name} hook`, () => {
       });
 
       it('should return the expected session object', async () => {
-        const { result } = setupUseCurrentSession({ profile });
+        const { result } = setupUseCurrentSession({ profiles: [profile] });
 
         expect(result.current.loading).toBe(true);
 
@@ -114,7 +116,7 @@ describe(`Given the ${useCurrentSession.name} hook`, () => {
     });
 
     it('should return the profile data without flickering of the "loading" flag', async () => {
-      const { result } = setupUseCurrentSession({ profile });
+      const { result } = setupUseCurrentSession({ profiles: [profile] });
 
       act(() => {
         updateSession(authenticatedProfile(wallet, profileIdentifier));
@@ -136,21 +138,38 @@ describe(`Given the ${useCurrentSession.name} hook`, () => {
   });
 
   describe(`when the user switches to a different profile`, () => {
-    const prevProfile = mockProfileFragment();
-    const prevWallet = mockWalletData({ address: prevProfile.ownedBy });
+    const prevProfile = mockProfileFragment({ ownedBy: wallet.address });
     const prevProfileIdentifier = mockProfileIdentifier({
       id: prevProfile.id,
       handle: prevProfile.handle,
     });
 
     beforeAll(() => {
-      updateSession(authenticatedProfile(prevWallet, prevProfileIdentifier));
+      updateSession(notAuthenticated());
     });
 
     it('should return the new profile data', async () => {
-      const { result } = setupUseCurrentSession({ profile });
+      const { result } = setupUseCurrentSession({ profiles: [prevProfile, profile] });
 
       act(() => {
+        updateSession(authenticatedProfile(wallet, prevProfileIdentifier));
+      });
+
+      await waitFor(() => {
+        // previous profile loaded
+        expect(result.current.loading).toBe(false);
+        expect(result.current.data).toMatchObject({
+          type: SessionType.WithProfile,
+          wallet,
+          profile: {
+            __typename: 'Profile',
+            ...prevProfileIdentifier,
+          },
+        });
+      });
+
+      act(() => {
+        // change to a new profile
         updateSession(authenticatedProfile(wallet, profileIdentifier));
       });
 
