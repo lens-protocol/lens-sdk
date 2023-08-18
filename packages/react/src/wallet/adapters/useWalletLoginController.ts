09206763c3391d0bdd30b1e7691579c5d8ee44d4
Cesare Naldi <naldi.cesare@gmail.com>
2023-08-18 13:24:41 +0200

    Fixes issue with useWalletLogin never settling

--- packages/react/src/wallet/adapters/useWalletLoginController.ts
+++ packages/react/src/wallet/adapters/useWalletLoginController.ts
@@ -1,13 +1,33 @@
-import { WalletLoginRequest } from '@lens-protocol/domain/use-cases/wallets';
+import { ActiveProfileLoader } from '@lens-protocol/domain/use-cases/profile';
+import { WalletLogin, WalletLoginRequest } from '@lens-protocol/domain/use-cases/wallets';
 
 import { useSharedDependencies } from '../../shared';
 import { WalletLoginPresenter } from './WalletLoginPresenter';
 
 export function useWalletLoginController() {
-  const { profileCacheManager, walletLogin } = useSharedDependencies();
+  const {
+    activeProfileGateway,
+    credentialsFactory,
+    credentialsGateway,
+    profileCacheManager,
+    profileGateway,
+    walletFactory,
+    walletGateway,
+    sessionPresenter,
+  } = useSharedDependencies();
 
   return async (request: WalletLoginRequest) => {
     const loginPresenter = new WalletLoginPresenter(profileCacheManager);
+    const activeProfileLoader = new ActiveProfileLoader(profileGateway, activeProfileGateway);
+    const walletLogin = new WalletLogin(
+      walletFactory,
+      walletGateway,
+      credentialsFactory,
+      credentialsGateway,
+      loginPresenter,
+      activeProfileLoader,
+      sessionPresenter,
+    );
 
     await walletLogin.login(request);
 
