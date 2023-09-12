0d3e258818dd44c6e15aeffc6068aa2a5d06f1ae
Reece Johnson <reece.robert.johnson@gmail.com>
2023-09-12 15:55:56 +0100

    feat: who reacted

--- examples/node/scripts/reactions/whoReacted.ts
+++ examples/node/scripts/reactions/whoReacted.ts
@@ -1,33 +1,41 @@
-import { LensClient, PublicationReactionType, development } from "@lens-protocol/client";
+import {
+  ExplorePublicationsOrderByType,
+  LensClient,
+  PublicationReactionType,
+  development,
+} from "@lens-protocol/client";
 
 async function main() {
   const lensClient = new LensClient({
     environment: development,
   });
 
-  const feedResult = await lensClient.reactions.whoReacted({
-    where: {
-      for: "PROFILE_ID",
-    },
+  const profilesWhoReacted = await lensClient.reactions.fetch({
+    for: "PUBLICATION_ID",
     limit: 20,
   });
 
-  const feedItems = feedResult.unwrap().items;
+  console.log(
+    `Profiles who reacted to publication: ${JSON.stringify(profilesWhoReacted, null, 2)}`
+  );
 
-  // find first item without a reaction
-  const feedItem = feedItems.find((item) => !item.reactions);
-
-  if (!feedItem) {
-    throw new Error(`You reacted to all feed items`);
-  }
-
-  // add reaction
   await lensClient.reactions.add({
     for: "PUBLICATION_ID",
     reaction: PublicationReactionType.Upvote,
   });
 
-  console.log(`You added a reaction to ${feedItem.root.id}`);
+  const profilesWhoReactedAfterUpvote = await lensClient.reactions.fetch({
+    for: "PUBLICATION_ID",
+    limit: 20,
+  });
+
+  console.log(
+    `Profiles who reacted to publication after upvote: ${JSON.stringify(
+      profilesWhoReactedAfterUpvote,
+      null,
+      2
+    )}`
+  );
 }
 
 main();
--- packages/client/src/submodules/publication/index.ts
+++ packages/client/src/submodules/publication/index.ts
@@ -1,3 +1,4 @@
 export * from './Publication';
 export * from './helpers';
 export * from './types';
+export * from './submodules';
