e2a9a8dcb3c9930e4eccd28ed1cf919e31c11c5e
Paweł Lula <pavloblack@hotmail.com>
2023-09-17 19:47:36 +0200

    Fix `node` example

--- examples/node/scripts/feed/forYou.ts
+++ examples/node/scripts/feed/forYou.ts
@@ -1,20 +1,21 @@
-import { LensClient, development } from '@lens-protocol/client';
-
-async function main() {
-  const client = new LensClient({
-    environment: development,
-  });
-
-  const profileId = 'PROFILE_ID';
-
-  const forYouFeedResult = await client.feed.forYou({
-    for: profileId,
-  });
-
-  console.log(`For you feed for ${profileId}`);
-  forYouFeedResult.unwrap().items.map((item) => {
-    console.log(`For you feed item`, JSON.stringify(item));
-  });
-}
-
-main();
+// `forYou` not yet ready to be exposed on production
+// import { LensClient, development } from '@lens-protocol/client';
+//
+// async function main() {
+//   const client = new LensClient({
+//     environment: development,
+//   });
+//
+//   const profileId = 'PROFILE_ID';
+//
+//   const forYouFeedResult = await client.feed.forYou({
+//     for: profileId,
+//   });
+//
+//   console.log(`For you feed for ${profileId}`);
+//   forYouFeedResult.unwrap().items.map((item) => {
+//     console.log(`For you feed item`, JSON.stringify(item));
+//   });
+// }
+//
+// main();
