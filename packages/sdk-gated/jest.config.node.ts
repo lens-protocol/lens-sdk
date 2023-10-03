3e47cec58c0ceebf9a6f1b05cca95fef33a169a9
Cesare Naldi <naldi.cesare@gmail.com>
2023-10-03 11:25:36 +0200

    Removes legacy sdk-gated

--- packages/sdk-gated/jest.config.node.ts
+++
@@ -1,7 +0,0 @@
-import config from './jest.config.browser';
-
-export default {
-  ...config,
-  setupFilesAfterEnv: ['./tests/setup-node.ts'],
-  testEnvironment: 'node',
-};
--- packages/sdk-gated/tests/utils.js
+++
@@ -1,2 +0,0 @@
-export const randomPath = () =>
-  '/' + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
