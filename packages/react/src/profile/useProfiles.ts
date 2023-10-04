018d3ae0c4fc1ec40fe2da5dc43e3e2c42ed75f9
Kris Urbas <605420+krzysu@users.noreply.github.com>
2023-10-04 16:10:28 +0200

    add examples

--- packages/react/src/profile/useProfiles.ts
+++ packages/react/src/profile/useProfiles.ts
@@ -18,6 +18,66 @@ export type UseProfilesArgs = PaginatedArgs<ProfilesRequest>;
  *
  * @category Profiles
  * @group Hooks
+ *
+ * @example
+ * Fetch profiles by handles
+ * ```tsx
+ * const { data, loading, error } = useProfiles({
+ *   where: {
+ *     handles: ['test/@firstprofile'],
+ *   },
+ * });
+ * ```
+ *
+ * @example
+ * Fetch profiles by ids
+ * ```tsx
+ * const { data, loading, error } = useProfiles({
+ *   where: {
+ *     profileIds: ['0x01', '0x02'],
+ *   },
+ * });
+ * ```
+ *
+ * @example
+ * Fetch profiles by owner addresses
+ * ```tsx
+ * const { data, loading, error } = useProfiles({
+ *   where: {
+ *     ownedBy: ['0xe3D871d389BF78c091E29deCe83200E9d6B2B0C2'],
+ *   },
+ * });
+ * ```
+ *
+ * @example
+ * Fetch profiles who commented on a publication
+ * ```tsx
+ * const { data, loading, error } = useProfiles({
+ *   where: {
+ *     whoCommentedOn: '0x1b-0x012b',
+ *   },
+ * });
+ * ```
+ *
+ * @example
+ * Fetch profiles who mirrored a publication
+ * ```tsx
+ * const { data, loading, error } = useProfiles({
+ *   where: {
+ *     whoMirroredPublication: '0x1b-0x012b',
+ *   },
+ * });
+ * ```
+ *
+ * @example
+ * Fetch profiles who quoted a publication
+ * ```tsx
+ * const { data, loading, error } = useProfiles({
+ *   where: {
+ *     whoQuotedPublication: '0x1b-0x012b',
+ *   },
+ * });
+ * ```
  */
 export function useProfiles({
   where,
