5d20ea6de77a281e817b4d7ea9f347b7389551ba
Reece Johnson <reece.robert.johnson@gmail.com>
2023-11-09 16:09:30 +0000

    fix: getting signer if incorrect chain id

--- packages/wagmi/src/index.ts
+++ packages/wagmi/src/index.ts
@@ -1,7 +1,6 @@
 import { IBindings } from '@lens-protocol/react-web';
 import { invariant } from '@lens-protocol/shared-kernel';
 import { providers } from 'ethers';
-import { polygon, polygonMumbai } from 'viem/chains';
 import { PublicClient, SwitchChainNotSupportedError, WalletClient } from 'wagmi';
 import { getNetwork, getPublicClient, getWalletClient, switchNetwork } from 'wagmi/actions';
 
@@ -17,48 +16,6 @@ function providerFromPublicClient({
     ensAddress: chain.contracts?.ensRegistry?.address,
   };
   return new providers.Web3Provider(transport, network);
-}
-
-async function addChainAndSwitch({
-  walletClient,
-  chainId,
-}: {
-  walletClient: WalletClient;
-  chainId: number;
-}) {
-  try {
-    switch (chainId) {
-      case polygonMumbai.id:
-        await walletClient.addChain({ chain: polygonMumbai });
-        break;
-      case polygon.id:
-        await walletClient.addChain({ chain: polygon });
-        break;
-      default:
-        // other chains are not supported
-        break;
-    }
-  } catch (err) {
-    // cannot add chain
-  }
-}
-
-async function switchChain({
-  walletClient,
-  chainId,
-}: {
-  walletClient: WalletClient;
-  chainId: number;
-}) {
-  try {
-    await switchNetwork({ chainId });
-  } catch (err) {
-    await addChainAndSwitch({ walletClient, chainId });
-    // best effort to switch network, if it fails, we just ignore it
-    if (!(err instanceof SwitchChainNotSupportedError)) {
-      throw err;
-    }
-  }
 }
 
 async function signerFromWalletClient({
@@ -92,10 +49,14 @@ export function bindings(): IBindings {
         const { chain } = getNetwork();
 
         if (chain?.id !== chainId) {
-          const walletClient = await getWalletClient();
-          invariant(walletClient, 'Wallet client not found');
-          await switchChain({ walletClient, chainId });
-          return signerFromWalletClient({ walletClient, chainId });
+          try {
+            await switchNetwork({ chainId });
+          } catch (err) {
+            // best effort to switch network, if it fails, we just ignore it
+            if (!(err instanceof SwitchChainNotSupportedError)) {
+              throw err;
+            }
+          }
         }
       }
 
