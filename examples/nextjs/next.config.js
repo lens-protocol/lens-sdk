8edff522dd3a11132e8c823f3630687d868254d4
Reece Johnson <reece.robert.johnson@gmail.com>
2023-11-15 15:36:39 +0000

    feat: remove old nextjs example, use workspace packages

--- examples/nextjs/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // Until the @apollo-client fixes the ESM modules support (https://github.com/apollographql/apollo-feature-requests/issues/287)
-  // it's required to either transpile the `@lens-protocol` packages or make sure they won't get `imported` during SSR.
-  transpilePackages: ['@lens-protocol'],
-};
-
-module.exports = nextConfig;
