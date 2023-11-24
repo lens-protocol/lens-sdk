f6af1f92f9623674657d12fb70da86d24241ea8e
Kris Urbas <605420+krzysu@users.noreply.github.com>
2023-11-24 11:39:12 +0100

    chore: add example script for public open action (#695)

---
+++ examples/node/scripts/authentication/authenticateWithWallet.ts
@@ -0,0 +1,31 @@
+import { LensClient, development } from '@lens-protocol/client';
+
+import { setupWallet } from '../shared/setupWallet';
+
+async function main() {
+  const client = new LensClient({
+    environment: development,
+  });
+
+  const wallet = setupWallet();
+  const address = await wallet.getAddress();
+
+  const { id, text } = await client.authentication.generateChallenge({
+    signedBy: address,
+  });
+  const signature = await wallet.signMessage(text);
+
+  await client.authentication.authenticate({ id, signature });
+
+  const accessTokenResult = await client.authentication.getAccessToken();
+  const accessToken = accessTokenResult.unwrap();
+
+  const walletAddress = await client.authentication.getProfileId();
+
+  console.log(`Is LensClient authenticated? `, await client.authentication.isAuthenticated());
+  console.log(`Authenticated wallet: `, walletAddress);
+  console.log(`Access token: `, accessToken);
+  console.log(`Is access token valid? `, await client.authentication.verify(accessToken));
+}
+
+main();
