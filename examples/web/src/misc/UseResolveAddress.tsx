7288e59963eff6a9e519da4c8f9b4630b06ba8e0
Cesare Naldi <naldi.cesare@gmail.com>
2023-12-20 22:24:39 +0100

    feat: adds useResolveAddress hook

---
+++ examples/web/src/misc/UseResolveAddress.tsx
@@ -0,0 +1,29 @@
+import { useResolveAddress } from '@lens-protocol/react-web';
+import toast from 'react-hot-toast';
+
+export function UseResolveAddress() {
+  const { execute, loading } = useResolveAddress();
+
+  const resolve = async () => {
+    const result = await execute({ handle: 'test/wagmi' });
+
+    if (result.isFailure()) {
+      toast.error(result.error.message);
+      return;
+    }
+
+    toast.success(`Resolved address: ${String(result.value)}`);
+  };
+
+  return (
+    <div>
+      <h1>
+        <code>useNotifications</code>
+      </h1>
+
+      <button onClick={resolve} disabled={loading}>
+        Resolve test/wagmi
+      </button>
+    </div>
+  );
+}
