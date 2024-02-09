f7c676da27197e522e0950d3f380a6eb78215eaf
Cesare Naldi <naldi.cesare@gmail.com>
2024-02-09 14:25:30 +0100

    Amends NextJS example

---
+++ examples/next-js/src/components/Providers.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { LensConfig, LensProvider, development } from "@lens-protocol/react-web";
+import { bindings } from "@lens-protocol/wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider, createConfig, http } from "wagmi";
+import { polygon, polygonMumbai } from "wagmi/chains";
+
+const queryClient = new QueryClient();
+
+const wagmiConfig = createConfig({
+  chains: [polygonMumbai, polygon],
+  transports: {
+    [polygonMumbai.id]: http(),
+    [polygon.id]: http(),
+  },
+});
+
+const lensConfig: LensConfig = {
+  environment: development,
+  bindings: bindings(wagmiConfig),
+};
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <LensProvider config={lensConfig}>{children}</LensProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
