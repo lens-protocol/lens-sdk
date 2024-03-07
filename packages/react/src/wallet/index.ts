38e528ab736f00f875bcccadc30503f6152a7633
Kris Urbas <605420+krzysu@users.noreply.github.com>
2024-03-07 14:32:03 +0100

    react: add useRateLimits hook

--- packages/react/src/wallet/index.ts
+++ packages/react/src/wallet/index.ts
@@ -6,5 +6,10 @@ export * from './useLastLoggedInProfile';
 export * from './useLazyProfilesManaged';
 export * from './useOwnedHandles';
 export * from './useProfilesManaged';
+export * from './useRateLimits';
 
-export type { ClaimableProfilesResult, HandleInfo } from '@lens-protocol/api-bindings';
+export type {
+  ClaimableProfilesResult,
+  HandleInfo,
+  UserCurrentRateLimitResult,
+} from '@lens-protocol/api-bindings';
