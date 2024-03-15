98051ae02fee31e859ad8ad4814fd5ce186b42b8
Cesare Naldi <naldi.cesare@gmail.com>
2024-03-15 12:25:14 +0100

    Improves IUploader interface

--- packages/react/src/transactions/publications/PublicationMetadataUploader.ts
+++ packages/react/src/transactions/publications/PublicationMetadataUploader.ts
@@ -8,50 +8,32 @@ export class PublicationMetadataUploader {
   constructor(private readonly uploader: IUploader) {}
 
   async upload(metadata: PublicationMetadata): PromiseResult<URI, UploadError> {
-    const updated = await this.prepare(metadata);
-
-    if (updated.isFailure()) {
-      return updated;
-    }
-
-    const blob = new Blob([JSON.stringify(updated.value)], { type: 'application/json' });
-    const file = new File([blob], 'metadata.json');
-
-    const single = await this.uploader.addFile(file);
-
-    if (single.isFailure()) {
-      return single;
-    }
-
-    const completion = await this.uploader.finalize();
-
-    if (completion.isFailure()) {
-      return completion;
-    }
-
-    return single;
-  }
-
-  private async prepare(
-    metadata: PublicationMetadata,
-  ): PromiseResult<PublicationMetadata, UploadError> {
-    const paths = resolveResourcePaths(metadata);
-
     try {
-      const updated = await update(metadata, paths, async (value, path) => {
-        const result = await this.uploader.addURI(path, value);
-        return result.unwrap();
-      });
+      const file = await this.prepare(metadata);
+      const uri = await this.uploader.addFile(file);
 
-      return success(updated);
+      await this.uploader.finalize();
+
+      return success(uri);
     } catch (err) {
       assertError(err);
 
       if (err instanceof UploadError) {
         return failure(err);
       }
-
       return failure(new UploadError('Failed to upload metadata', { cause: err }));
     }
+  }
+
+  private async prepare(metadata: PublicationMetadata): Promise<File> {
+    const paths = resolveResourcePaths(metadata);
+
+    const updated = await update(metadata, paths, async (uri, path) =>
+      this.uploader.addURI(path, uri),
+    );
+
+    const blob = new Blob([JSON.stringify(updated)], { type: 'application/json' });
+
+    return new File([blob], 'metadata.json');
   }
 }
