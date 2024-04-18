48af288d6357c76cc664a0d33e22fa47ddcf7cff
Kris Urbas <605420+krzysu@users.noreply.github.com>
2024-04-18 11:12:19 +0200

    fix lens next app e2e test

--- examples/lens-next-app/playwright.config.ts
+++ examples/lens-next-app/playwright.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "pnpm dev",
+    command: "npm run dev",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
   },
