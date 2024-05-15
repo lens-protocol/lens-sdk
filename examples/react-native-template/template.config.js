d86477c0e95dca1c75c752b7bb97d558c42744f3
Kris Urbas <605420+krzysu@users.noreply.github.com>
2024-05-15 11:28:08 +0200

    react: add lens react native template

---
+++ examples/react-native-template/template.config.js
@@ -0,0 +1,14 @@
+/**
+ * Init a new React Native project with the Lens template
+ *
+ * ```bash
+ * $ npx @react-native-community/cli init LensApp --template https://github.com/lens-protocol/lens-sdk/examples/react-native-template
+ * $ npx @react-native-community/cli init LensApp --template https://github.com/lens-protocol/lens-sdk/tree/react-native-template/examples/react-native-template
+ * ```
+ * */
+
+module.exports = {
+  placeholderName: "HelloWorld",
+  titlePlaceholder: "Hello App Display Name",
+  templateDir: ".",
+};
