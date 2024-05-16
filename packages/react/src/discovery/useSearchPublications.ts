be49f3efb58944fa2801884cd119bd6ede51a8a8
Cesare Naldi <naldi.cesare@gmail.com>
2024-05-16 16:38:38 +0200

    feat: add React Suspense support to useSearchPublications hook

--- packages/react/src/discovery/useSearchPublications.ts
+++ packages/react/src/discovery/useSearchPublications.ts
@@ -1,23 +1,37 @@
 import {
   PrimaryPublication,
   PublicationSearchRequest,
-  useSearchPublications as useBaseSearchPublications,
+  PublicationSearchWhere,
+  SearchPublicationsDocument,
 } from '@lens-protocol/api-bindings';
 
 import { useLensApolloClient } from '../helpers/arguments';
-import { PaginatedArgs, PaginatedReadResult, usePaginatedReadResult } from '../helpers/reads';
+import { PaginatedArgs, PaginatedReadResult } from '../helpers/reads';
+import {
+  SuspendablePaginatedResult,
+  SuspenseEnabled,
+  SuspensePaginatedResult,
+  useSuspendablePaginatedQuery,
+} from '../helpers/suspense';
 import { useFragmentVariables } from '../helpers/variables';
 
+/**
+ * {@link useSearchPublications} hook arguments
+ */
 export type UseSearchPublicationsArgs = PaginatedArgs<PublicationSearchRequest>;
+
+export type { PublicationSearchRequest, PublicationSearchWhere };
+
+/**
+ * {@link useSearchPublications} hook arguments with Suspense support
+ *
+ * @experimental This API can change without notice
+ */
+export type UseSuspenseSearchPublicationsArgs = SuspenseEnabled<UseSearchPublicationsArgs>;
 
 /**
  * Search for publications based on a defined criteria
  *
- * @category Discovery
- * @group Hooks
- * @param args - {@link UseSearchPublicationsArgs}
- *
- * @example
  * Search for publications with content that contains "foo"
  * ```tsx
  * import { useSearchPublications } from '@lens-protocol/react';
@@ -41,7 +55,6 @@ export type UseSearchPublicationsArgs = PaginatedArgs<PublicationSearchRequest>;
  * }
  * ```
  *
- * @example
  * Search for audio post publications with content that matches a query
  * ```tsx
  * import { useSearchPublications } from '@lens-protocol/react';
@@ -72,22 +85,71 @@ export type UseSearchPublicationsArgs = PaginatedArgs<PublicationSearchRequest>;
  *   );
  * }
  * ```
+ *
+ * @category Discovery
+ * @group Hooks
  */
+export function useSearchPublications(
+  args: UseSearchPublicationsArgs,
+): PaginatedReadResult<PrimaryPublication[]>;
+
+/**
+ * Search for publications based on a defined criteria
+ *
+ * This signature supports [React Suspense](https://react.dev/reference/react/Suspense).
+ *
+ * ```tsx
+ * const { data } = useSearchPublications({
+ *   query: 'foo',
+ *   suspense: true,
+ * });
+ *
+ * console.log(data);
+ * ```
+ *
+ * Use [startTransition](https://react.dev/reference/react/startTransition) to avoid to re-suspend the component.
+ *
+ * ```tsx
+ * const [query, setQuery] = useState('foo');
+ *
+ * const { data } = useSearchPublications({
+ *   query,
+ *   suspense: true,
+ * });
+ *
+ * const search = () => {
+ *   startTransition(() => {
+ *     setQuery('bar');
+ *   });
+ * };
+ * ```
+ *
+ * @experimental This API can change without notice
+ * @category Discovery
+ * @group Hooks
+ */
+export function useSearchPublications(
+  args: UseSuspenseSearchPublicationsArgs,
+): SuspensePaginatedResult<PrimaryPublication[]>;
+
 export function useSearchPublications({
-  query,
   limit,
+  query,
+  suspense = false,
   where,
-}: UseSearchPublicationsArgs): PaginatedReadResult<PrimaryPublication[]> {
-  return usePaginatedReadResult(
-    useBaseSearchPublications(
-      useLensApolloClient({
-        variables: useFragmentVariables({
-          query,
-          limit,
-          where,
-          statsFor: where?.metadata?.publishedOn,
-        }),
+}: UseSearchPublicationsArgs & { suspense?: boolean }): SuspendablePaginatedResult<
+  PrimaryPublication[]
+> {
+  return useSuspendablePaginatedQuery({
+    suspense,
+    query: SearchPublicationsDocument,
+    options: useLensApolloClient({
+      variables: useFragmentVariables({
+        query,
+        limit,
+        where,
+        statsFor: where?.metadata?.publishedOn,
       }),
-    ),
-  );
+    }),
+  });
 }
