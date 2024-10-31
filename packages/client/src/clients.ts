a24472493535cb025e62603cfabd62327fc878e8
Cesare Naldi <naldi.cesare@gmail.com>
2024-10-31 12:10:16 +0100

    fix: introduces Paginated<T>

--- packages/client/src/clients.ts
+++ packages/client/src/clients.ts
@@ -38,14 +38,7 @@ import {
   hasExtensionCode,
 } from './errors';
 import { decodeIdToken } from './tokens';
-
-/**
- * A standardized data object.
- *
- * All GQL operations should alias their results to `value` to ensure interoperability
- * with this client interface.
- */
-export type StandardData<T> = { value: T };
+import type { StandardData } from './types';
 
 function takeValue<T>({
   data,
