2fe376915f3a3733c06bb6e61e075e8add9b15fe
Cesare Naldi <naldi.cesare@gmail.com>
2024-12-04 10:34:38 +0100

    fix: missing use of new config

--- packages/types/src/misc.ts
+++ packages/types/src/misc.ts
@@ -14,3 +14,14 @@ export type Cursor = Tagged<string, 'Cursor'>;
  * A DateTime string in ISO 8601 format.
  */
 export type DateTime = Tagged<string, 'DateTime'>;
+
+/**
+ * Beautify the  readout of all of the members of that intersection.
+ *
+ * @see https://twitter.com/mattpocockuk/status/1622730173446557697
+ *
+ * @internal
+ */
+export type Prettify<T> = {
+  [K in keyof T]: T[K];
+} & {};
