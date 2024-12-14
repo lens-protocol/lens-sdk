b21b32b8d3b70ee7bd06694256a5104dda2f032f
Juan Garcia <juan@avara.xyz>
2024-12-14 11:19:40 +0100

    Add missing role

--- packages/graphql/src/enums.ts
+++ packages/graphql/src/enums.ts
@@ -1,4 +1,14 @@
 /**
+ * Enum for the different authentication roles a user can have.
+ */
+export enum Role {
+  AccountOwner = 'ACCOUNT_OWNER',
+  AccountManager = 'ACCOUNT_MANAGER',
+  OnboardingUser = 'ONBOARDING_USER',
+  Builder = 'BUILDER',
+}
+
+/**
  * Enum for AccessConditionComparison.
  */
 export enum AccessConditionComparison {
