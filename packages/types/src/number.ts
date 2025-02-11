23d185c7b1952c36123bd00b35abd40963710c13
Cesare Naldi <naldi.cesare@gmail.com>
2025-02-11 20:17:17 +0100

    feat: bigNumber helper

--- packages/types/src/number.ts
+++ packages/types/src/number.ts
@@ -1,9 +1,14 @@
 import type { Tagged } from 'type-fest';
+import { invariant } from './helpers';
 
 /**
  * A string representation of an high precision decimal number (e.g., "0.1", "1000.42")
  */
 export type BigDecimal = Tagged<string, 'BigDecimal'>;
+export function bigDecimal(value: string): BigDecimal {
+  invariant(!/^-?\d+(\.\d+)?$/.test(value), `Invalid BigDecimal: ${value}`);
+  return value as BigDecimal;
+}
 
 /**
  * A string representation of a big integer number.
