296119174803718f33014d568dd612ab643dab06
Cesare Naldi <naldi.cesare@gmail.com>
2025-02-25 21:12:04 +0100

    fix: use existing signer/walletClient in thirdweb pay example

--- examples/thirdweb-pay/src/App.tsx
+++ examples/thirdweb-pay/src/App.tsx
@@ -1,42 +1,60 @@
+import 'viem/window';
+import { chains } from '@lens-network/sdk/viem';
 import { createThirdwebClient } from 'thirdweb';
-import { ethereum, sepolia } from 'thirdweb/chains';
+import { viemAdapter } from 'thirdweb/adapters/viem';
+import { ethereum } from 'thirdweb/chains';
 import { PayEmbed } from 'thirdweb/react';
+import { type Address, createWalletClient, custom } from 'viem';
 
-// // hoist account
-// const [address] = (await window.ethereum!.request({ method: 'eth_requestAccounts' })) as [Address];
+const chain = chains.testnet;
 
-// const walletClient = createWalletClient({
-//   account: address,
-//   chain,
-//   transport: custom(window.ethereum!),
-// });
+// hoist account
+const [address] = (await window.ethereum!.request({ method: 'eth_requestAccounts' })) as [Address];
+
+const walletClient = createWalletClient({
+  account: address,
+  chain: chain,
+  transport: custom(window.ethereum!),
+});
+
+const thirdwebWallet = await viemAdapter.wallet.fromViem({
+  walletClient: walletClient,
+});
 
 const client = createThirdwebClient({ clientId: '44323e7868feac3bd3ea4d91c9e879d4' });
+
+await thirdwebWallet.connect({ client });
 
 export function App() {
   return (
     <PayEmbed
+      activeWallet={thirdwebWallet}
       client={client}
       payOptions={{
-        buyWithFiat: { preferredProvider: 'COINBASE', testMode: true },
+        buyWithFiat: {
+          preferredProvider: 'COINBASE',
+          testMode: true, // enable test mode
+        },
         buyWithCrypto: false,
         prefillBuy: {
           token: {
             // Using GHO on Ethereum to get quotes working
-            address: '0x40d16fc0246ad3160ccc09b8d0d3a2cd28ae6c2f', // GHO on Ethereum
-            // address: '0x2Be68B15c693D3b5747F9F0D49D30A2E81BAA2Df', // GRASS on Sepolia
+            address: '0x40d16fc0246ad3160ccc09b8d0d3a2cd28ae6c2f',
 
             // Making it look like GRASS token
             name: 'GRASS',
             symbol: 'GRASS',
             icon: 'https://block-explorer.testnet.lens.dev/images/grass.png',
           },
-          chain: ethereum,
+          chain: ethereum, // workaround for getting quotes working
           allowEdits: {
             amount: true, // allow editing buy amount
             token: false, // disable selecting buy token
             chain: false, // disable selecting buy chain
           },
+        },
+        onPurchaseSuccess: (purchase) => {
+          console.log('Purchase success', purchase);
         },
       }}
     />
