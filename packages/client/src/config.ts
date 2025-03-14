8c7cc0ec19c287f617f40d9d765a8f91f2e492e0
Juan <juan@avara.xyz>
2025-03-14 18:29:56 +0100

    Update config.ts

--- packages/client/src/config.ts
+++ packages/client/src/config.ts
@@ -37,7 +37,7 @@ export type ClientConfig = {
 
   /**
    * The Server API Key for your Lens App.
-   * 
+   *
    * Use this with a Server API Key to not incur in rate limits when used on a server-to-server scenario.
    */
   apiKey?: string;
