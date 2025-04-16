0e3b37d28b8ffa6acf8fdcb7172264d2f5ad6bee
Juan Garcia <juan@avara.xyz>
2025-04-16 10:11:32 +0200

    chore: update schema to add accountScore filter for posts query

--- packages/client/src/actions/sns.ts
+++ packages/client/src/actions/sns.ts
@@ -11,24 +11,24 @@ import {
 } from '@lens-protocol/graphql';
 import type { ResultAsync } from '@lens-protocol/types';
 
-import type { AnyClient, SessionClient } from '../clients';
+import type { SessionClient } from '../clients';
 import type { UnauthenticatedError, UnexpectedError } from '../errors';
 
 /**
  * Fetch a SNS subscription.
  *
  * ```ts
- * const result = await fetchSnsSubscription(anyClient, {
+ * const result = await fetchSnsSubscription(sessionClient, {
  *   account: evmAddress('0xe2f2a5C287993345a840db3B0845fbc70f5935a5'),
  * });
  * ```
  *
- * @param client - Any Lens client.
+ * @param client - The session client logged as a builder.
  * @param request - The query request.
  * @returns The details of the SNS subscription.
  */
 export function fetchSnsSubscription(
-  client: AnyClient,
+  client: SessionClient,
   request: GetSnsSubscriptionsRequest,
 ): ResultAsync<Array<SnsSubscription> | [], UnexpectedError> {
   return client.query(GetSnsSubscriptionsQuery, { request });
