843e897d825b00d28cd650f8cf29e5f3f6f86dbb
Cesare Naldi <naldi.cesare@gmail.com>
2025-04-16 21:52:08 +0200

    chore: disable cross-region grove tests

--- packages/client/src/crossRegion.test.ts
+++ packages/client/src/crossRegion.test.ts
@@ -13,71 +13,48 @@ import { handleOperationWith } from './viem';
 const storageClient = storage.StorageClient.create(storage.staging);
 const acl = storage.lensAccountOnly(TEST_ACCOUNT, 37111);
 
-describe('Given an instance of the StorageClient (bound to staging)', { timeout: 10000 }, () => {
-  let initialFileResponse: storage.FileUploadResponse;
-  let newAccount: Account;
-  let sessionClient: SessionClient;
+describe.skip(
+  'Given an instance of the StorageClient (bound to staging)',
+  { timeout: 10000 },
+  () => {
+    let initialFileResponse: storage.FileUploadResponse;
+    let newAccount: Account;
+    let sessionClient: SessionClient;
 
-  const updates = metadata.account({
-    name: 'Bruce Wayne',
-  });
-
-  beforeEach(async () => {
-    const initialMetadata = metadata.account({
-      name: 'John Doe',
+    const updates = metadata.account({
+      name: 'Bruce Wayne',
     });
-    initialFileResponse = await storageClient.uploadAsJson(initialMetadata, { acl });
-    await initialFileResponse.waitForPropagation();
-    const result = await loginAsOnboardingUser().andThen((sessionClient) =>
-      createAccountWithUsername(sessionClient, {
-        username: { localName: `testname${Date.now()}` },
-        metadataUri: initialFileResponse.gatewayUrl,
-      })
-        .andThen(handleOperationWith(wallet))
-        .andThen(sessionClient.waitForTransaction)
-        .andThen((txHash) => fetchAccount(sessionClient, { txHash }))
-        .andThen((account) => {
-          newAccount = account ?? never('Account not found');
-          return sessionClient.switchAccount({
-            account: newAccount.address,
-          });
-        }),
-    );
 
-    assertOk(result);
-    sessionClient = result.value;
-  }, 20000);
-
-  describe('When I upload a file from one region', () => {
-    it('Then it should be accessible from the Lens API in another region', async () => {
-      const response = await storageClient.uploadAsJson(updates, {
-        acl: storage.immutable(CHAIN.id),
+    beforeEach(async () => {
+      const initialMetadata = metadata.account({
+        name: 'John Doe',
       });
-
-      const result = await setAccountMetadata(sessionClient, {
-        metadataUri: response.gatewayUrl,
-      })
-        .andThen(handleOperationWith(wallet))
-        .andThen(sessionClient.waitForTransaction)
-        .andThen((_) => fetchAccount(sessionClient, { address: newAccount.address }));
+      initialFileResponse = await storageClient.uploadAsJson(initialMetadata, { acl });
+      await initialFileResponse.waitForPropagation();
+      const result = await loginAsOnboardingUser().andThen((sessionClient) =>
+        createAccountWithUsername(sessionClient, {
+          username: { localName: `testname${Date.now()}` },
+          metadataUri: initialFileResponse.gatewayUrl,
+        })
+          .andThen(handleOperationWith(wallet))
+          .andThen(sessionClient.waitForTransaction)
+          .andThen((txHash) => fetchAccount(sessionClient, { txHash }))
+          .andThen((account) => {
+            newAccount = account ?? never('Account not found');
+            return sessionClient.switchAccount({
+              account: newAccount.address,
+            });
+          }),
+      );
 
       assertOk(result);
+      sessionClient = result.value;
+    }, 20000);
 
-      expect(result.value).toMatchObject({
-        metadata: {
-          name: updates.lens.name,
-        },
-      });
-    });
-  });
-
-  describe('When I edit a file from one region', () => {
-    it(
-      'Then it should be accessible from the Lens API in another region',
-      { timeout: 30000 },
-      async () => {
-        const response = await storageClient.updateJson(initialFileResponse.uri, updates, wallet, {
-          acl,
+    describe('When I upload a file from one region', () => {
+      it('Then it should be accessible from the Lens API in another region', async () => {
+        const response = await storageClient.uploadAsJson(updates, {
+          acl: storage.immutable(CHAIN.id),
         });
 
         const result = await setAccountMetadata(sessionClient, {
@@ -88,31 +65,63 @@ describe('Given an instance of the StorageClient (bound to staging)', { timeout:
           .andThen((_) => fetchAccount(sessionClient, { address: newAccount.address }));
 
         assertOk(result);
+
         expect(result.value).toMatchObject({
           metadata: {
             name: updates.lens.name,
           },
         });
-      },
-    );
-  });
+      });
+    });
 
-  describe('When I delete a file from one region', () => {
-    it(
-      'Then it should no longer be accessible from the Lens API in another region',
-      { timeout: 20000 },
-      async () => {
-        const response = await storageClient.delete(initialFileResponse.uri, wallet);
-        expect(response).toHaveProperty('success', true);
+    describe('When I edit a file from one region', () => {
+      it(
+        'Then it should be accessible from the Lens API in another region',
+        { timeout: 30000 },
+        async () => {
+          const response = await storageClient.updateJson(
+            initialFileResponse.uri,
+            updates,
+            wallet,
+            {
+              acl,
+            },
+          );
 
-        const result = await setAccountMetadata(sessionClient, {
-          metadataUri: initialFileResponse.uri,
-        })
-          .andThen(handleOperationWith(wallet))
-          .andThen(sessionClient.waitForTransaction);
+          const result = await setAccountMetadata(sessionClient, {
+            metadataUri: response.gatewayUrl,
+          })
+            .andThen(handleOperationWith(wallet))
+            .andThen(sessionClient.waitForTransaction)
+            .andThen((_) => fetchAccount(sessionClient, { address: newAccount.address }));
 
-        assertErr(result);
-      },
-    );
-  });
-});
+          assertOk(result);
+          expect(result.value).toMatchObject({
+            metadata: {
+              name: updates.lens.name,
+            },
+          });
+        },
+      );
+    });
+
+    describe('When I delete a file from one region', () => {
+      it(
+        'Then it should no longer be accessible from the Lens API in another region',
+        { timeout: 20000 },
+        async () => {
+          const response = await storageClient.delete(initialFileResponse.uri, wallet);
+          expect(response).toHaveProperty('success', true);
+
+          const result = await setAccountMetadata(sessionClient, {
+            metadataUri: initialFileResponse.uri,
+          })
+            .andThen(handleOperationWith(wallet))
+            .andThen(sessionClient.waitForTransaction);
+
+          assertErr(result);
+        },
+      );
+    });
+  },
+);
