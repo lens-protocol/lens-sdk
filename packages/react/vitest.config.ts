d3f270f2ec870f5bdf35faba220d0eb9c4660a9a
Cesare Naldi <naldi.cesare@gmail.com>
2025-06-03 11:55:35 +0200

    chore: update tests setup based on vitest 3.2.x

--- packages/react/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { resolve } from 'node:path';
-import { defineProject } from 'vitest/config';
-
-export default defineProject({
-  test: {
-    environment: 'happy-dom',
-    setupFiles: [resolve(__dirname, './vitest.setup.ts')],
-  },
-});
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,5 @@
 import { resolve } from 'node:path';
 import { loadEnv } from 'vite';
-// import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
@@ -8,8 +7,43 @@ export default defineConfig({
   test: {
     setupFiles: [resolve(__dirname, './vitest.setup.ts')],
     env: loadEnv('', process.cwd(), ''),
-    environment: 'node',
     testTimeout: 10_000,
     hookTimeout: 15_000,
+    fileParallelism: false,
+    projects: [
+      {
+        extends: true,
+        test: {
+          name: 'react',
+          environment: 'happy-dom',
+          setupFiles: [resolve(__dirname, './packages/react/vitest.setup.ts')],
+          include: ['packages/react/**/*.{unit,integration}.test.{ts,tsx}'],
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: 'client',
+          include: ['packages/client/**/*.{unit,integration}.test.{ts,tsx}'],
+          environment: 'node',
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: 'storage',
+          include: ['packages/storage/**/*.{unit,integration}.test.{ts,tsx}'],
+          environment: 'node',
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: 'e2e',
+          include: ['packages/**/*.{e2e}.test.{ts,tsx}'],
+          environment: 'node',
+        },
+      },
+    ],
   },
 });
