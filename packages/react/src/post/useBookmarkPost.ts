ca88fa42971e8ba7bbc38310db589c32dc375364
Juan Garcia <juan@avara.xyz>
2025-06-24 11:03:39 +0200

    feat: add hooks for bookmark a post

---
+++ packages/react/src/post/useBookmarkPost.ts
@@ -0,0 +1,37 @@
+import type {
+  BookmarkPostRequest,
+  UnauthenticatedError,
+  UnexpectedError,
+} from '@lens-protocol/client';
+import { bookmarkPost } from '@lens-protocol/client/actions';
+
+import { type UseAsyncTask, useAuthenticatedAsyncTask } from '../helpers';
+
+/**
+ * Bookmark a post.
+ *
+ * @alpha This is an alpha API and may be subject to breaking changes.
+ *
+ * ```tsx
+ * import { evmAddress, useBookmarkPost } from '@lens-protocol/react';
+ *
+ * const { execute } = useBookmarkPost();
+ *
+ * const result = await execute({
+ *   post: postId('42'),
+ * });
+ *
+ * if (result.isErr()) {
+ *   console.error(result.error);
+ * }
+ * ```
+ */
+export function useBookmarkPost(): UseAsyncTask<
+  BookmarkPostRequest,
+  void,
+  UnauthenticatedError | UnexpectedError
+> {
+  return useAuthenticatedAsyncTask((sessionClient, request) =>
+    bookmarkPost(sessionClient, request),
+  );
+}
