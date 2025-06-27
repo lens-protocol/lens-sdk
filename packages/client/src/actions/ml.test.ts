df6a709fad68a3d667525afa37964635d87cb88f
Juan Garcia <juan@avara.xyz>
2025-06-27 10:09:56 +0200

    fix:lint

--- packages/client/src/actions/ml.test.ts
+++ packages/client/src/actions/ml.test.ts
@@ -1,23 +1,22 @@
 import { assertOk } from '@lens-protocol/types';
 import { beforeAll, describe, it } from 'vitest';
-
+import type { SessionClient } from '../clients';
 import { loginAsAccountOwner, TEST_ACCOUNT } from '../test-utils';
 import {
   fetchAccountRecommendations,
   fetchPostsForYou,
   fetchPostsToExplore,
 } from './ml';
-import { SessionClient } from '../clients';
 
 describe('Given the ML query actions', () => {
   let sessionClient: SessionClient;
-  
+
   beforeAll(async () => {
     await loginAsAccountOwner().andTee((client) => {
       sessionClient = client;
     });
   });
-  
+
   // TODO: Enable when bug fixed LENS-1201
   describe.skip(`When invoking the '${fetchAccountRecommendations.name}' action`, () => {
     it('Then it should not fail w/ a GQL BadRequest error', async () => {
