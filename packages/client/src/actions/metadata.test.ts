ffff397b904c381f98a3b9ea61871916712722da
Juan Garcia <juan@avara.xyz>
2025-07-03 10:51:55 +0200

    fix: make sure metadata is propagated before edit

--- packages/client/src/actions/metadata.test.ts
+++ packages/client/src/actions/metadata.test.ts
@@ -33,7 +33,10 @@ describe('Given a Lens Post', () => {
     );
     assertOk(result);
     item = result.value;
-  });
+
+    // Make sure the metadata is on IPFS before being able to edit/delete it
+    await resources.waitForPropagation();
+  }, 20_000);
 
   describe(`When the metadata at 'contentUri' is updated`, () => {
     const client = createPublicClient();
@@ -42,7 +45,7 @@ describe('Given a Lens Post', () => {
     beforeAll(async () => {
       const response = await updateTextOnlyMetadata(item.contentUri, updates);
       await response.waitForPropagation();
-    }, 30_000);
+    }, 20_000);
 
     it('Then it should be possible to force a refresh of the metadata', async () => {
       const refreshed = await refreshMetadata(client, {
