794ac3e38ea8a356dc575bfda898281449ad2f28
Juan Garcia <juan@avara.xyz>
2025-07-07 14:48:39 +0200

    feat: add useEnableSignless and useRemoveSignless to react hooks

---
+++ packages/react/src/account/useRemoveSignless.ts
@@ -0,0 +1,41 @@
+import type {
+  OperationHandler,
+  SigningError,
+  TransactionIndexingError,
+  UnauthenticatedError,
+  UnexpectedError,
+  ValidationError
+} from '@lens-protocol/client';
+import { removeSignless } from '@lens-protocol/client/actions';
+
+import { fetchMeDetails } from '@lens-protocol/client/actions';
+import type { TxHash } from '@lens-protocol/types';
+import { type UseAsyncTask, useAuthenticatedAsyncTask } from '../helpers';
+
+export type UseRemoveSignlessArgs = {
+  handler: OperationHandler;
+};
+
+/**
+ * Remove signless for the authenticated Account.
+ *
+ * @alpha This is an alpha API and may be subject to breaking changes.
+ */
+export function useRemoveSignless(
+  args: UseRemoveSignlessArgs,
+): UseAsyncTask<
+  void,
+  TxHash,
+  | SigningError
+  | ValidationError
+  | TransactionIndexingError
+  | UnauthenticatedError
+  | UnexpectedError
+> {
+  return useAuthenticatedAsyncTask((sessionClient) =>
+    removeSignless(sessionClient)
+      .andThen(args.handler)
+      .andThen(sessionClient.waitForTransaction)
+      .andTee(() => fetchMeDetails(sessionClient))
+  );
+}
